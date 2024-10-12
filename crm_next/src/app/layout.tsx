c3d5f3ccb2935198fb74958a27d806b4b0712c92
Dmytro Reviakin <thefaust31@gmail.com>
2024-10-12 18:40:24 +0300

    add path

--- crm_next/src/app/layout.tsx
+++ crm_next/src/app/layout.tsx
@@ -1,4 +1,7 @@
 import './globals.css';
+import {Plus_Jakarta_Sans} from 'next/font/google'
+
+const font = Plus_Jakarta_Sans({subsets:['latin']})
 
 export default function RootLayout({
   children,
@@ -7,9 +10,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={font.className}>
         {children}
       </body>
     </html>
   );
 }
+import './globals.css';
+
+
+
